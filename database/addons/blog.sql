ae504b2fb4f8f4705ace5cd89fafc35a755d2b38
Mike Schekotov <mschekotov@cs-cart.com>
2017-02-22 10:09:59 +0400

    [~] {@18210} Blog pages IDs fixed. Emails fixed.

--- database/addons/blog.sql
+++ database/addons/blog.sql
@@ -1,17 +1,17 @@
-DELETE FROM ?:page_descriptions WHERE page_id IN (SELECT page_id FROM ?:pages WHERE page_type = 'B') AND page_id NOT IN (28, 29, 30, 31, 33);
-DELETE FROM ?:pages WHERE page_type = 'B' AND page_id NOT IN (28, 29, 30, 31, 33);
-DELETE FROM ?:blog_authors WHERE page_id NOT IN (28, 29, 30, 31, 33);
+DELETE FROM ?:page_descriptions WHERE page_id IN (SELECT page_id FROM ?:pages WHERE page_type = 'B') AND page_id NOT IN (33, 34, 35, 36, 37);
+DELETE FROM ?:pages WHERE page_type = 'B' AND page_id NOT IN (33, 34, 35, 36, 37);
+DELETE FROM ?:blog_authors WHERE page_id NOT IN (33, 34, 35, 36, 37);
 
-REPLACE INTO ?:pages (`page_id`, `company_id`, `parent_id`, `id_path`, `status`, `page_type`, `position`, `timestamp`, `usergroup_ids`, `localization`, `new_window`, `use_avail_period`, `avail_from_timestamp`, `avail_till_timestamp`, `facebook_obj_type`) VALUES (28,0,33,'33/28','A','B',0,1485291600,'0','',0,'N',0,0,'activity');
-REPLACE INTO ?:pages (`page_id`, `company_id`, `parent_id`, `id_path`, `status`, `page_type`, `position`, `timestamp`, `usergroup_ids`, `localization`, `new_window`, `use_avail_period`, `avail_from_timestamp`, `avail_till_timestamp`, `facebook_obj_type`) VALUES (29,0,33,'33/29','A','B',0,1485291600,'0','',0,'N',0,0,'activity');
-REPLACE INTO ?:pages (`page_id`, `company_id`, `parent_id`, `id_path`, `status`, `page_type`, `position`, `timestamp`, `usergroup_ids`, `localization`, `new_window`, `use_avail_period`, `avail_from_timestamp`, `avail_till_timestamp`, `facebook_obj_type`) VALUES (30,0,33,'33/30','A','B',0,1485291600,'0','',0,'N',0,0,'activity');
-REPLACE INTO ?:pages (`page_id`, `company_id`, `parent_id`, `id_path`, `status`, `page_type`, `position`, `timestamp`, `usergroup_ids`, `localization`, `new_window`, `use_avail_period`, `avail_from_timestamp`, `avail_till_timestamp`, `facebook_obj_type`) VALUES (31,0,33,'33/31','A','B',0,1485291600,'0','',0,'N',0,0,'activity');
 REPLACE INTO ?:pages (`page_id`, `company_id`, `parent_id`, `id_path`, `status`, `page_type`, `position`, `timestamp`, `usergroup_ids`, `localization`, `new_window`, `use_avail_period`, `avail_from_timestamp`, `avail_till_timestamp`, `facebook_obj_type`) VALUES (33,0,0,'33','A','B',0,1485291600,'0','',0,'N',0,0,'activity');
+REPLACE INTO ?:pages (`page_id`, `company_id`, `parent_id`, `id_path`, `status`, `page_type`, `position`, `timestamp`, `usergroup_ids`, `localization`, `new_window`, `use_avail_period`, `avail_from_timestamp`, `avail_till_timestamp`, `facebook_obj_type`) VALUES (34,0,33,'33/34','A','B',0,1485291600,'0','',0,'N',0,0,'activity');
+REPLACE INTO ?:pages (`page_id`, `company_id`, `parent_id`, `id_path`, `status`, `page_type`, `position`, `timestamp`, `usergroup_ids`, `localization`, `new_window`, `use_avail_period`, `avail_from_timestamp`, `avail_till_timestamp`, `facebook_obj_type`) VALUES (35,0,33,'33/35','A','B',0,1485291600,'0','',0,'N',0,0,'activity');
+REPLACE INTO ?:pages (`page_id`, `company_id`, `parent_id`, `id_path`, `status`, `page_type`, `position`, `timestamp`, `usergroup_ids`, `localization`, `new_window`, `use_avail_period`, `avail_from_timestamp`, `avail_till_timestamp`, `facebook_obj_type`) VALUES (36,0,33,'33/36','A','B',0,1485291600,'0','',0,'N',0,0,'activity');
+REPLACE INTO ?:pages (`page_id`, `company_id`, `parent_id`, `id_path`, `status`, `page_type`, `position`, `timestamp`, `usergroup_ids`, `localization`, `new_window`, `use_avail_period`, `avail_from_timestamp`, `avail_till_timestamp`, `facebook_obj_type`) VALUES (37,0,33,'33/37','A','B',0,1485291600,'0','',0,'N',0,0,'activity');
 
-REPLACE INTO ?:blog_authors (`page_id`, `user_id`) VALUES (28,1);
-REPLACE INTO ?:blog_authors (`page_id`, `user_id`) VALUES (29,1);
-REPLACE INTO ?:blog_authors (`page_id`, `user_id`) VALUES (30,1);
-REPLACE INTO ?:blog_authors (`page_id`, `user_id`) VALUES (31,1);
 REPLACE INTO ?:blog_authors (`page_id`, `user_id`) VALUES (33,1);
+REPLACE INTO ?:blog_authors (`page_id`, `user_id`) VALUES (34,1);
+REPLACE INTO ?:blog_authors (`page_id`, `user_id`) VALUES (35,1);
+REPLACE INTO ?:blog_authors (`page_id`, `user_id`) VALUES (36,1);
+REPLACE INTO ?:blog_authors (`page_id`, `user_id`) VALUES (37,1);
 
 
