cb6ca2f12734e33b5aafada8b8b5aec88c1e2172
ushaev1510 <aushaev@cs-cart.com>
2018-10-11 17:34:00 +0400

    [~]{@f_1-20229} Demo data has changed

--- database/addons/product_variations.sql
+++ database/addons/product_variations.sql
@@ -1,1 +1,1 @@
-UPDATE ?:products_categories SET category_id = 413 WHERE product_id = 277;
+UPDATE ?:products_categories SET category_id = 413 WHERE product_id IN (277, 278, 279, 280, 281, 282, 283);
