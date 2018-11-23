d0763c68fe89ba9d9527fc7b8688cef41c162491
Aleksandr Scherbachenko <ascherbachenko@cs-cart.com>
2018-11-23 16:15:14 +0400

    [!] {@22489} Mobile app: layout: banners urls changed to work in mobile app + product id change (to one from demo data)

--- database/addons/banners.sql
+++ database/addons/banners.sql
@@ -4,11 +4,12 @@ UPDATE ?:banners SET position=20 WHERE banner_id=9;
 UPDATE ?:banners SET position=30 WHERE banner_id=16;
 UPDATE ?:banners SET position=40 WHERE banner_id=18;
 
-UPDATE ?:banner_descriptions SET banner='Welcome to Multi-Vendor demo marketplace', url='pages.view&page_id=24', description='' WHERE banner_id=6;
-UPDATE ?:banner_descriptions SET banner='Acme Corporation: Join our loyalty program to get special prices', url='pages.view&page_id=25', description='' WHERE banner_id=8;
-UPDATE ?:banner_descriptions SET banner='Stark Industries: buy two for the price of one', url='pages.view&page_id=26', description='' WHERE banner_id=9;
-UPDATE ?:banner_descriptions SET banner='Become our vendor with no transaction fee', url='pages.view&page_id=27', description='' WHERE banner_id=16;
+UPDATE ?:banner_descriptions SET banner='Welcome to Multi-Vendor demo marketplace', url='index.php?dispatch=pages.view&page_id=24', description='' WHERE banner_id=6;
+UPDATE ?:banner_descriptions SET banner='Acme Corporation: Join our loyalty program to get special prices', url='index.php?dispatch=pages.view&page_id=25', description='' WHERE banner_id=8;
+UPDATE ?:banner_descriptions SET banner='Stark Industries: buy two for the price of one', url='index.php?dispatch=pages.view&page_id=26', description='' WHERE banner_id=9;
+UPDATE ?:banner_descriptions SET banner='Become our vendor with no transaction fee', url='index.php?dispatch=pages.view&page_id=27', description='' WHERE banner_id=16;
 UPDATE ?:banner_descriptions SET banner='No transaction fee', url='companies.apply_for_vendor&plan_id=2', description='' WHERE banner_id=18;
+UPDATE ?:banner_descriptions SET url='index.php?dispatch=products.view&product_id=5764' WHERE banner_id=17;
 
 UPDATE ?:images_links SET image_id=8270 WHERE object_type='promo' AND object_id IN (SELECT banner_image_id FROM ?:banner_images WHERE banner_id=6);
 UPDATE ?:images_links SET image_id=8277 WHERE object_type='promo' AND object_id IN (SELECT banner_image_id FROM ?:banner_images WHERE banner_id=8);
