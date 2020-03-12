b2672f3d025b62a029585757b5dee4294f46861c
Pavel Shalunov <shalunov.pasha@gmail.com>
2020-03-12 11:59:20 +0400

    [~]{1-25713} Added banners for mobile version

--- database/addons/banners.sql
+++ database/addons/banners.sql
@@ -13,9 +13,9 @@ UPDATE ?:banner_descriptions SET banner='Stark Industries: buy two for the price
 UPDATE ?:banner_descriptions SET banner='Become our vendor with no transaction fee', url='index.php?dispatch=pages.view&page_id=27', description='' WHERE banner_id=16;
 UPDATE ?:banner_descriptions SET banner='No transaction fee', url='companies.apply_for_vendor&plan_id=2', description='' WHERE banner_id=18;
 UPDATE ?:banner_descriptions SET url='index.php?dispatch=products.view&product_id=5764' WHERE banner_id=17;
-UPDATE ?:banner_descriptions SET banner='X-Box Mobi', url='index.php?dispatch=products.view&product_id=5764', description='' WHERE banner_id=20;
-UPDATE ?:banner_descriptions SET banner='Acme Mobi', url='index.php?dispatch=pages.view&page_id=25', description='' WHERE banner_id=21;
-UPDATE ?:banner_descriptions SET banner='Multivendor demo mobi', url='index.php?dispatch=pages.view&page_id=24', description='' WHERE banner_id=22;
+UPDATE ?:banner_descriptions SET banner='X-Box Mobile', url='index.php?dispatch=products.view&product_id=5764', description='' WHERE banner_id=20;
+UPDATE ?:banner_descriptions SET banner='Acme Mobile', url='index.php?dispatch=pages.view&page_id=25', description='' WHERE banner_id=21;
+UPDATE ?:banner_descriptions SET banner='Multivendor demo mobile', url='index.php?dispatch=pages.view&page_id=24', description='' WHERE banner_id=22;
 
 UPDATE ?:images_links SET image_id=8270 WHERE object_type='promo' AND object_id IN (SELECT banner_image_id FROM ?:banner_images WHERE banner_id=6);
 UPDATE ?:images_links SET image_id=8277 WHERE object_type='promo' AND object_id IN (SELECT banner_image_id FROM ?:banner_images WHERE banner_id=8);
